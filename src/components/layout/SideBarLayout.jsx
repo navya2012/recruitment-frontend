edd123cdbb6d6cf0eeb5e74eba40e8aa613e6903
navya2012 <navyapadala1995@gmail.com>
2024-10-03 21:35:54 +0530

    done changes

--- src/components/layout/SideBarLayout.jsx
+++ src/components/layout/SideBarLayout.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Box } from '@mui/material';
 import { Outlet } from 'react-router-dom';
-import Footer from '../footer/Footer';
 import SideBar from '../sidebar/SideBar';
 
 
